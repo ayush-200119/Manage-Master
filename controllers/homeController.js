cf5f7fd22e07ca6339f45e94fadf4204343b2241
unknown <ayushsalvi19@gmail.com>
2022-07-07 20:59:39 +0530

    Implement the create-a-new-todo feature

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -17,9 +17,43 @@ router.get("/:userid/todos",function(req,res){
     Student.findOne({id:uid},function(err,foundStudent){
         if(!err)
         {
-            res.render("todos",{todos:foundStudent.todos});
+            res.render("todos",{todos:foundStudent.todos,userid:uid});
         }
     });
+});
+
+router.post("/:userid/todos",function(req,res){
+    const uid=req.params.userid;
+    const currTitle=req.body.todotitle;
+    const currTodo={title:currTitle,checklist:[]};
+    
+    //updating the database of the current user and redirecting to the required route
+    Student.findOneAndUpdate({'student.id':uid},{$push:{todos:currTodo}},function(err){
+        if(!err)
+            console.log("Successfully updated");
+    });
+    res.redirect(`/${uid}/todos/${currTitle}`);
+});
+
+//individual to-do
+router.get("/:userid/todos/:todo",function(req,res){
+    //find the user with the given id and get the todo list and find the required to-do from it
+    const uid=req.params.userid;
+    const todoTitle=req.params.todo;
+
+    function checkTitle(todo)
+    {
+        return todo.title===todoTitle;
+    }
+
+    Student.findOne({id:uid},function(err,foundStudent){
+        if(!err)
+        {
+            const requiredTodo=foundStudent.todos.find(checkTitle);
+            res.render("todo",{title:requiredTodo.title,checklist:requiredTodo.checklist});
+        }
+    });
+
 });
 
 router.get("/:userid/notes",function(req,res){
